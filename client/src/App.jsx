6281bc4389a319ca29d4a490144af957c8c5fbad
Jin <passawit53@gmail.com>
2025-03-28 09:40:02 +0700

    project setup : web structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return <div className="text-3xl font-bold underline text-red-500">
+  Hello world!
+</div>;
 }
 
 export default App;
